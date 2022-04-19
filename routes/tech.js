9bc651686d9d39fc04539cf5954faa9e7d833fd2
MakCatt <maksat.oraz.95@gmail.com>
2022-04-20 04:05:59 +0600

    midka back end

---
+++ routes/tech.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+
+router
+    .route("/")
+    .get((req, res) => res.sendFile(__dirname+"/teachers.html"))
+
+module.exports = router;
