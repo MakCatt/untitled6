1f971bbf29bd391953ee6262c5bf02d87b16e80a
MakCatt <maksat.oraz.95@gmail.com>
2022-04-20 06:16:03 +0600

    back

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router
     .route("/")
-    .get((req, res) => res.sendFile(__dirname+"/login.ejs"))
+    .get((req, res) => res.sendFile(__dirname+"/login.html"))
 
 module.exports = router;
